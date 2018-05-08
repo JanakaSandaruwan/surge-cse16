bf89556cba3bc21a05e07db665d7e31548194f98
JanakaSandaruwan <janakasandaruwan1996@gmail.com>
2018-05-08 16:06:18 +0530

    change side bar

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -15,12 +15,47 @@ export class TeacherComponent implements OnInit {
   username: string;
   usercode: string;
   fulldata : object = <object>{} ;
+  state : string = "expanded";
+  dropdownstate : string = "collapsed";
+//  username: string;
+  profileurl: string;
   constructor(private logincookie : CookieService , private route: ActivatedRoute, private login : LoginServiceService,private router:Router){}//,private home:TeacherHomeComponent) { }
+
+  movedown(){
+      console.log("dad");
+      if (this.dropdownstate == "collapsed"){
+        $('.dropdown-menu').first().stop(true, true).slideDown();
+        this.dropdownstate = "down";
+      }else{
+        $('.dropdown-menu').first().stop(true, true).slideUp();
+        this.dropdownstate = "collapsed";
+      }
+
+  }
+  togglenav(){
+    if (this.state == "expanded") {
+        $('.sidebar').css('margin-left', '-190px');
+        $('#main-wrapper').css('margin-left', '60px');
+        $('.menu-icon').css('float','none');
+        $('.menu-icon').css('position','absolute');
+        $('.menu-icon').css('right','0');
+        this.state = "minimized";
+    } else {
+        if (this.state == "minimized") {
+            $('.sidebar').css('margin-left', '0px');
+            $('#main-wrapper').css('margin-left', '250px');
+            $('.menu-icon').css('float','left');
+            $('.menu-icon').css('position','relative');
+            this.state = "expanded";
+        }
+    }
+  }
 
   ngOnInit() {
     if (!this.logincookie.get("uname")){
       this.router.navigate(['']);
     }
+    
     console.log(this.logincookie.get("uname")+"adsa");
     if (this.login.getloginstatus(this.logincookie.get("uname")) == false){
       this.router.navigate(['']);
