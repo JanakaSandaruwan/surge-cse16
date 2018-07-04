44376b90c87424f60b376024acab3a3c49a6d0d7
unknown <madushanwmd@gmail.com>
2018-07-04 04:55:41 -0700

    update 7

--- src/app/student/my-course/my-course.component.ts
+++ src/app/student/my-course/my-course.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {StudentservicesService} from '../../services/studentservices.service';
+import {LocalStorageService, SessionStorageService} from 'ngx-webstorage';
 @Component({
   selector: 'app-my-course',
   templateUrl: './my-course.component.html',
   styleUrls: ['./my-course.component.css']
 })
 export class MyCourseComponent implements OnInit {
-
-
-module = [
-
-
-];
-  constructor() { }
+  Level : string;
+  ModuleList : any[] = [];
+  constructor(private storage:LocalStorageService,private _sservice : StudentservicesService) { }
 
   ngOnInit() {
   }
-
+  showl2(){
+    console.log("component");
+    this._sservice.levelModule(this.storage.retrieve("uname"),"Year 2").subscribe(data => {
+      this.ModuleList = data;
+      console.log(this.ModuleList[0].module.moduleName);
+});
+}
 }
