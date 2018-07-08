9dca49aa5476e69d65b9468dd500a9126d698ee3
unknown <madushanwmd@gmail.com>
2018-07-08 09:53:28 -0700

    final update

--- src/app/student/my-course/my-course.component.ts
+++ src/app/student/my-course/my-course.component.ts
@@ -18,59 +18,51 @@ export class MyCourseComponent implements OnInit {
   constructor(private router:Router, private route: ActivatedRoute,private storage:LocalStorageService,private _sservice : StudentservicesService) { }
 
   ngOnInit() {
+    this.Level = this._sservice.getLevel(this.storage.retrieve("uname"));
+    console.log(this.Level);
   }
-  showl1(){
+  showl1(level){
     console.log("component");
+    if(this.Level=="Year 1"){
     this._sservice.levelModule(this.storage.retrieve("uname"),"Year 1").subscribe(data => {
       this.ModuleListl1 = data;
       //console.log(this.ModuleListl1[0].module.moduleName);
+
 });
 }
-  showl2(){
-    console.log("component");
+}
+  showl2(level){
+    if(this.Level=="Year 2"){
     this._sservice.levelModule(this.storage.retrieve("uname"),"Year 2").subscribe(data => {
       this.ModuleListl2 = data;
       //console.log(this.ModuleListl2);
+
 });
+}
 }
 showl3(){
   console.log("component");
+  if(this.Level=="Year 3"){
   this._sservice.levelModule(this.storage.retrieve("uname"),"Year 3").subscribe(data => {
     this.ModuleListl3 = data;
     //console.log(this.ModuleListl3[0].module.moduleName);
 });
+}
+}
+showl4(){
+  console.log("component");
+  if(this.Level=="Year 4"){
+  this._sservice.levelModule(this.storage.retrieve("uname"),"Year 4").subscribe(data => {
+    this.ModuleListl4 = data;
+    //console.log(this.ModuleListl3[0].module.moduleName);
+});
+}
 }
 view(subject){
   console.log("dimuthu");
   console.log(subject);
   this.router.navigate(['./student/subject',{subjectname: btoa(subject),details:btoa(this.storage.retrieve("uname"))}]);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
